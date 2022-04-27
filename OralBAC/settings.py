2dca027a305b4409aaa4f493127f5297c44bb3e5
NotJude <95127080+NotJude@users.noreply.github.com>
2022-04-27 20:47:20 +0200

    Update settings.py

--- OralBAC/settings.py
+++ OralBAC/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%5&%n=k&z2dmgl6hnj-7v333*i3#fhe1v*@h4$_wyvwpx2=q!9'
+SECRET_KEY = 'secret_key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
