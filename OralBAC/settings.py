66ba5655a94bce4fa17917e1368cb793a5f7b363
Jude <jude.gautier05@orange.fr>
2022-04-28 22:33:39 +0200

    rrrr

--- OralBAC/settings.py
+++ OralBAC/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'secret_key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['oralbacfrancaisdjango.herokuapp.com', '127.0.0.1']
 
--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
 web: gunicorn OralBAC.wsgi --log-file -
+release: python manage.py migrate
                                
