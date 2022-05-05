4bbe06529fdace9e04dbab4f7c3542042edaeff0
Jude <jude.gautier05@orange.fr>
2022-05-05 22:23:00 +0200

    adding

--- Presentation/forms.py
+++ Presentation/forms.py
@@ -1,8 +1,13 @@
+# S/O to philgyford : 
+# https://github.com/philgyford/django-nested-inline-formsets-example/tree/main/publishing/books
 
-from django.forms import ModelForm, ModelChoiceField, inlineformset_factory
+
+from django.forms import ModelForm, ModelChoiceField, inlineformset_factory, BaseInlineFormSet
+from django.utils.translation import gettext_lazy as _
 
 from .models import Texte, Auteur, Mouvement, Ligne
-from .utils import RelatedFieldWidgetCanAdd
+from .utils import RelatedFieldWidgetCanAdd, is_empty_form, is_form_persisted
+
 
 class AuteurForm(ModelForm):
     class Meta:
@@ -10,13 +15,7 @@ class AuteurForm(ModelForm):
         fields = '__all__'
 
 
-class MouvementForm(ModelForm):
-    class Meta:
-        model = Mouvement
-        exclude = ('texte', )
-
-
-class TexteForm(ModelForm):
+"""class TexteForm(ModelForm):
     
     auteur = ModelChoiceField(
        required=False,
@@ -26,13 +25,125 @@ class TexteForm(ModelForm):
 
     class Meta:
         model = Texte
-        fields = '__all__'
-
-MouvementInlineFormSet = inlineformset_factory(Texte, Mouvement, exclude=('texte',), extra=2)
-
-LigneInlineFormSet = inlineformset_factory(Mouvement, Ligne, exclude=('mouvement',), extra=1)
+        fields = '__all__'"""
 
 
 
+# The formset for editing the Ligne that belong to a Mouvement.
+LigneFormset = inlineformset_factory(
+    Ligne, Mouvement, exclude=('child', ), extra=4
+)
+
+
+class BaseMouvementFormset(BaseInlineFormSet):
+    """
+    The base formset for editing Mouvements belonging to a Texte, and the
+    Ligne belonging to those Mouvements.
+    """
+
+    def add_fields(self, form, index):
+        super().add_fields(form, index)
+
+        # Save the formset for a Child's Images in the nested property.
+        form.nested = LigneFormset(
+            instance=form.instance,
+            data=form.data if form.is_bound else None,
+            files=form.files if form.is_bound else None,
+            prefix="Address-%s-%s"
+            % (form.prefix, LigneFormset.get_default_prefix()),
+        )
+
+    def is_valid(self):
+        """
+        Also validate the nested formsets.
+        """
+        result = super().is_valid()
+
+        if self.is_bound:
+            for form in self.forms:
+                if hasattr(form, "nested"):
+                    result = result and form.nested.is_valid()
+
+        return result
+
+    def clean(self):
+        """
+        If a parent form has no data, but its nested forms do, we should
+        return an error, because we can't save the parent.
+        For example, if the Mouvement form is empty, but there are Lignes.
+        """
+        super().clean()
+
+        for form in self.forms:
+            if not hasattr(form, "nested") or self._should_delete_form(form):
+                continue
+
+            if self._is_adding_nested_inlines_to_empty_form(form):
+                form.add_error(
+                    field=None,
+                    error=_(
+                        "Vous êtes en train d'essayer d'ajouter une Ligne à "
+                        "un Mouvement qui n'existe pas encore. Veuillez "
+                        "ajouter des informations à propos de ce(s) "
+                        "Mouvement(s) et remplissez de nouveau les Lignes"
+                    ),
+                )
+
+    def save(self, commit=True):
+        """
+        Also save the nested formsets.
+        """
+        result = super().save(commit=commit)
+
+        for form in self.forms:
+            if hasattr(form, "nested"):
+                if not self._should_delete_form(form):
+                    form.nested.save(commit=commit)
+
+        return result
+
+    def _is_adding_nested_inlines_to_empty_form(self, form):
+        """
+        Are we trying to add data in nested inlines to a form that has no data?
+        e.g. Adding Lignes to a new Mouvement whose data we haven't entered?
+        """
+        if not hasattr(form, "nested"):
+            # A basic form; it has no nested forms to check.
+            return False
+
+        if is_form_persisted(form):
+            # We're editing (not adding) an existing model.
+            return False
+
+        if not is_empty_form(form):
+            # The form has errors, or it contains valid data.
+            return False
+
+        # All the inline forms that aren't being deleted:
+        non_deleted_forms = set(form.nested.forms).difference(
+            set(form.nested.deleted_forms)
+        )
+
+        # At this point we know that the "form" is empty.
+        # In all the inline forms that aren't being deleted, are there any that
+        # contain data? Return True if so.
+        return any(not is_empty_form(nested_form) for nested_form in non_deleted_forms)
+
+
+# This is the formset for the Mouvements belonging to a Texte and the
+# Lignes belonging to those Mouvements.
+#
+# You'd use this by passing in a Texte:
+#     TexteMouvementFormset(**form_kwargs, instance=self.object)
+ChildrenFormset = inlineformset_factory(
+    Texte,
+    Mouvement,
+    formset=BaseMouvementFormset,
+    # We need to specify at least one Mouvement field:
+    exclude=('parent',),
+    extra=3,
+    # If you don't want to be able to delete Textes:
+    # can_delete=False
+)
 
 
--- Presentation/views.py
+++ Presentation/views.py
@@ -2,9 +2,9 @@
 # from django.forms import inlineformset_factory
 from django.shortcuts import render, get_object_or_404, redirect
 
-from .models import Mouvement, Texte, Ligne
+from .models import Texte
 from .filters import TexteFilter
-from .forms import *
+from .forms import AuteurForm
 
 
 def index(request):
@@ -27,55 +27,10 @@ def texte(request, key):
         }
     return render(request, 'Presentation/texte.html', context)
 
-'''
-def add(request):
-    # ajouter un texte et des mouvements 
-    
-    if request.method == 'POST': 
-        #print(1)
-        tform = TexteForm(request.POST)
-        mformslist = [(MouvementForm(request.POST),LigneInlineFormSet(request.POST, request.FILES)) for _ in range(6)]
-        lformset = LigneInlineFormSet(request.POST, request.FILES)
-        #print(2)
-        if tform.is_valid():
-            new_texte = tform.save()
-            #print(3)
-            for mf, lformset in mformslist:
 
-                new_mvt = mf.save(commit=False)
-                #print(4)
-                #print(new_mvt.title)
-                if new_mvt.title + new_mvt.range != '':
-                    
-                    #print(5)
-                    new_mvt.texte = new_texte
-                    new_mvt.save()
-                    if lformset.is_valid():
-                        #print(6)
-                        for lf in lformset:
-                            new_ligne = lf.save(commit=False)
-                            #print(7)
-                            if new_ligne.citation + new_ligne.outil + new_ligne.analyse !='':
-                                #print(8)
-                                new_ligne.mouvement = new_mvt
-                                new_ligne.save()
-            
-            new_text = tform.save()
-            for mform in mformslist:
-                new_mvt = mform.save(commit=False)
-                if new_mvt.title + new_mvt.range != '':
-                    new_mvt.texte = new_text
-                    new_mvt.save()
-                
-            return redirect('Presentation:texte', key=tform.instance.url)
-    
-    else: 
-        tform = TexteForm()
-        mformslist = [(MouvementForm(), LigneInlineFormSet()) for _ in range(6)]
-        lformset = LigneInlineFormSet()
-    #            modifier la template 
-    return render(request, 'Presentation/add.html', {'tform':tform, 'mformslist':mformslist, 'lformset':lformset})
-'''
+def add(request):
+    texte = get_object_or_404(Texte, title="Vénus Anadyomène")
+    return render(request, 'Presentation/add.html')
 
 
 
